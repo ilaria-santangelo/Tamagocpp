bd2cf313b46250e0b0e48cef238c09d6d9abe5f0
Sebastian Paz <spazrocha@gmail.com>
2023-06-22 12:14:33 +0200

    updated folder and gitignore

---
+++ Source Files/SymbolTable.cpp
@@ -0,0 +1,108 @@
+#include "../Header Files/SymbolTable.h"
+#include <fstream>
+#include <iostream>
+
+SymbolTable::SymbolTable() : head(nullptr) {}
+
+void SymbolTable::insert(std::string name, Pet* pet) {
+    SymbolTableEntry* newEntry = new SymbolTableEntry;
+    newEntry->name = name;
+    newEntry->pet = pet;
+    newEntry->next = head;
+    head = newEntry;
+}
+
+Pet* SymbolTable::lookup(std::string name) {
+    SymbolTableEntry* entry = head;
+    while (entry != nullptr) {
+        if (entry->name == name) {
+            return entry->pet;
+        }
+        entry = entry->next;
+    }
+    return nullptr;
+}
+
+void SymbolTable::save() {
+    std::ofstream file("pets.txt");
+    if (file.is_open()) {
+        SymbolTableEntry* entry = head;
+        while (entry != nullptr) {
+            file << entry->name << "\n";
+            file << entry->pet->saveState() << "\n";
+            entry = entry->next;
+        }
+        file.close();
+    } else {
+        std::cerr << "Unable to open file for saving";
+    }
+}
+
+void SymbolTable::load(const std::string& filename) {
+    clear();
+    std::cout << "Loading...\n";
+    std::ifstream file(filename);
+    if (file.is_open()) {
+        std::string line;
+        while (std::getline(file, line)) {
+            std::string petName = line;
+            if(std::getline(file, line)) {
+                Pet* pet = new Pet();
+                pet->loadState(line);
+                insert(petName, pet);
+            }
+        }
+        file.close();
+    } else {
+        std::cerr << "Unable to open file for loading";
+    }
+}
+
+void SymbolTable::clear() {
+    SymbolTableEntry* current = head;
+    while (current != nullptr) {
+        SymbolTableEntry* toDelete = current;
+        current = current->next;
+        delete toDelete->pet;
+        delete toDelete;
+    }
+    head = nullptr;
+}
+
+void SymbolTable::bury(std::string name) {
+    SymbolTableEntry* entry = head;
+    SymbolTableEntry* prev = nullptr;
+    while (entry != nullptr) {
+        if (entry->name == name) {
+            if (entry->pet->isAlive() == false) {
+                if (prev == nullptr) {
+                    head = entry->next;
+                } else {
+                    prev->next = entry->next;
+                }
+
+                // Delete the entry
+                delete entry->pet;
+                delete entry;
+                std::cout << "The pet " << name << " has been buried.\n";
+                return;
+            } else {
+                std::cout << "The pet " << name << " is still alive and cannot be buried.\n";
+                return;
+            }
+        }
+        prev = entry;
+        entry = entry->next;
+    }
+    std::cout << "The pet " << name << " does not exist.\n";
+}
+
+SymbolTable::~SymbolTable() {
+    while (head != nullptr) {
+        SymbolTableEntry* next = head->next;
+        delete head->pet;
+        delete head;
+        head = next;
+    }
+}
+
---
+++ cmake-build-debug-visual-studio/CMakeFiles/Tamagocpp.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/spazr/CLionProjects/Tamagocpp/Grammar Files/lexer.flex.cpp" "CMakeFiles/Tamagocpp.dir/Grammar_Files/lexer.flex.cpp.obj" "msvc" "CMakeFiles/Tamagocpp.dir/Grammar_Files/lexer.flex.cpp.obj.d"
+  "C:/Users/spazr/CLionProjects/Tamagocpp/Grammar Files/parser.tab.cpp" "CMakeFiles/Tamagocpp.dir/Grammar_Files/parser.tab.cpp.obj" "msvc" "CMakeFiles/Tamagocpp.dir/Grammar_Files/parser.tab.cpp.obj.d"
+  "C:/Users/spazr/CLionProjects/Tamagocpp/Source Files/Pet.cpp" "CMakeFiles/Tamagocpp.dir/Source_Files/Pet.cpp.obj" "msvc" "CMakeFiles/Tamagocpp.dir/Source_Files/Pet.cpp.obj.d"
+  "C:/Users/spazr/CLionProjects/Tamagocpp/Source Files/SymbolTable.cpp" "CMakeFiles/Tamagocpp.dir/Source_Files/SymbolTable.cpp.obj" "msvc" "CMakeFiles/Tamagocpp.dir/Source_Files/SymbolTable.cpp.obj.d"
+  "C:/Users/spazr/CLionProjects/Tamagocpp/Source Files/main.cpp" "CMakeFiles/Tamagocpp.dir/Source_Files/main.cpp.obj" "msvc" "CMakeFiles/Tamagocpp.dir/Source_Files/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
